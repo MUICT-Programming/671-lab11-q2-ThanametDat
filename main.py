7c37b86ad5659c4296e7084a23900c5fcde6336e
ThanametDat <thanamet789@gmail.com>
2024-10-19 20:27:19 +0700

    Tha

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 # YOUR CODE HERE
+
+def summation():
+    for i in range(n):
+        x = lst1[i]+lst2[i]
+        updated_list.append(x)
+    return print(updated_list)
+
+def find_min_max():
+    updated_list.sort()
+    return print(f'({updated_list[0]},{updated_list[-1]})')
+
+n=int(input())
+lst1=[]
+lst2=[]
+updated_list=[]
+
+for i in range(n):
+    x=int(input())
+    lst1.append(x)
+
+for i in range(n):
+    y=int(input())
+    lst2.append(y)
+
+summation()
+find_min_max()
